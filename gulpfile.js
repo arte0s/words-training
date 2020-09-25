cc7015a604fae55143ddae0431625b6942fa6ec2
Artem Sch <arteos@gmail.com>
2020-09-25 17:02:45 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+// var uglify = require('gulp-uglify');
+var minify = require('gulp-minify');
+
+gulp.task('js', function(){
+   return gulp.src(['js/*.js'])
+        .pipe(concat('index.js'))
+        .pipe(minify())
+        // .pipe(uglify())
+        .pipe(gulp.dest('.'));
+});
